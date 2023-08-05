bad959ffd4fe95e27cacec1862c3f7442c716774
ss-oms <ss.oms@yandex.ru>
2023-08-05 19:09:39 +0600

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    watchFiles: ["./src/index.html"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
