b91c2615bd3b4ae644444efd817a0ae45aa8c4a8
ss-oms <ss.oms@yandex.ru>
2023-08-05 20:59:01 +0600

    add react+ts

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,40 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[hash].js",
   },
   devServer: {
     port: 3000,
-    hot: true,
-    watchFiles: ["./src/index.html"],
   },
   plugins: [
     new HTMLWebpackPlugin({ template: "./src/index.html" }),
     new CleanWebpackPlugin(),
   ],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
       },
     ],
   },
